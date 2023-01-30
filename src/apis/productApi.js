f5bb98aaff958fef54bd73449534b28b357a6dd4
Arun Dinesh <arundinesh247@gmail.com>
2023-01-30 22:18:36 +0530

    baseurl comments

--- src/apis/productApi.js
+++ src/apis/productApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
   baseURL: "https://fakestoreapi.com/",
-  // Add base url from an env file if you prefer
+  // Use an env file to load a baseurl if you prefer that way
 });
